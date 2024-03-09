d04d0d02c9cfe958957da951c889bc0120d7c22d
Casey Tran <caseytran892@gmail.com>
2024-03-09 12:55:34 -0800

    camera diff

--- camera.py
+++ camera.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def takePhoto():
     print("Initializing video")
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
         
     print("Shaping video")
     # Shapes video for fisheye fix adjustment
@@ -50,7 +50,7 @@ def takePhoto():
             print(f"Taking photo {str(picNumber)}")
             # save the image
             # imageName = f"./gestures/none/{str(picNumber)}.jpg"
-            imageName = f"./segmentation/images/{str(picNumber)}.jpg"
+            imageName = f"./segmentation/{str(picNumber)}.jpg"
             cv2.imwrite(imageName, frame)
             picNumber += 1
     cap.release()
